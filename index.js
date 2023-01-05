74d64a96214142f02e2f4edfde5cf5b3e15cfe01
Morgan Frotten <mmfrotten@gmail.com>
2023-01-05 11:19:09 -0500

    server set up

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+const server = require('./api/server')
+server.listen(process.env.PORT || 9000, () => {
+    console.log('server started')
+})
